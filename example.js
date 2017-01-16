d1d5eb2b6702a5493e3e25dcf355dca2a5f1873f
danPixl <danPixl@users.noreply.github.com>
2017-01-15 17:53:09 -0700

    Fix the require

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-var leowiki = require('./leowiki');
+var leowiki = require('leowiki');
 
 leowiki.page(process.argv.slice(2).join(' '), function(err, text) { // run with arguments being page title
     if (err) throw err;
